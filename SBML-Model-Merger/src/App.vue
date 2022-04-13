a5dd1b6422a23324d811d77ed3b9de1ccc97c959
tgeb <tom.gebhardt@uni-rostock.de>
2022-04-13 21:08:46 +0200

    init git

---
+++ SBML-Model-Merger/src/App.vue
@@ -0,0 +1,154 @@
+<template>
+  <div id="topLayer" class="d-flex flex-column h-100">
+    <div class="custom-color-1" id="head">
+      <div class="container">
+        <div class="row">
+          <h1 class="col-8">Model Merger</h1>
+          <div class="col-4 d-flex align-items-center justify-content-end">
+            <button
+              type="button"
+              class="btn btn-light btn-sm"
+              v-on:click="btnSwitchView"
+              v-if="true"
+            >
+              Switch Mode
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="custom-color-2" id="intro">
+      <div class="container">
+        <div class="row justify-content-center">
+          <p class="col-8">
+            Model development is often a non-linear and decentralised process.
+            It requries time consuming effort to gather the necessary
+            information from literature, experimental data and databases. The
+            information are either collected in tables or directly put into a
+            model using common tools (e. g. CellDesigner). Subseqeuntly, the
+            collaborative nature of modelling requires one partner to collect
+            and combine the gathered information. Our merging tool aims to ease
+            this process by enabling the combination of two model versions.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="custom-color-2" id="center">
+      <div class="container">
+        <user-merge v-if="!simple" />
+        <simple-merge v-else />
+      </div>
+    </div>
+    <div class="footer mt-auto custom-color-3" id="foot">
+      <div class="container">
+        <!--Image-->
+        <div class="row">
+          <div class="col-2 d-flex align-items-center">
+            <a
+              href="https://integrative-pathway-models.de/"
+              title="INCOME-Website"
+            >
+              <img src="./assets/csm_logo_INCOME11.png" class="img-fluid" alt="INCOME-LOGO" />
+            </a>
+          </div>
+          <div class="col-10 d-flex align-items-end flex-column" id="contact">
+            <h5>Contact</h5>
+            <p>Tom Gebhardt</p>
+            <p>tom.gebhardt@uni-rostock.de</p>
+            <p>https://www.sbi.uni-rostock.de/</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 d-flex justify-content-start">
+            The project was funded by the BMBF (FKZ: 01ZX1705C)
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+//import Selection from './components/Selection.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import SimpleMerge from "./components/SimpleMerge.vue";
+import UserMerge from "./components/UserMerge.vue";
+//import { useRoute } from "vue-router";
+
+export default defineComponent ({
+  name: "app",
+  components: {
+    SimpleMerge,
+    UserMerge,
+  },
+  data() {
+    return {
+      simple: true,
+      job: "fakeJob",
+      //job: this.$route.query.jobID,
+    };
+  },
+  methods: {
+    btnSwitchView: function () {
+      this.simple = !this.simple;
+    },
+  },
+});
+
+// export default {
+//   name: 'App',
+//   components: {
+//     //HelloWorld,
+//     SimpleMerge,
+//     //Selection,
+//     UserMerge
+//   }
+// }
+</script>
+
+<style>
+.row > * {
+}
+</style>
+
+<style scoped>
+.custom-color-1 {
+  background-color: #365b9e;
+  color: #feffff;
+}
+
+.custom-color-2 {
+  background-color: #e5e5e5;
+  color: #17252a;
+}
+
+.custom-color-3 {
+  background-color: white;
+  color: #17252a;
+}
+
+#intro {
+  padding-top: 1rem;
+}
+
+#center {
+  padding-bottom: 1em;
+}
+
+#head {
+  padding-top: 1em;
+  padding-bottom: 1rem;
+}
+
+#foot {
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
+
+#contact > p {
+  margin-bottom: auto;
+}
+</style>
---
+++ SBML-Model-Merger/src/components/UserMerge.vue
@@ -0,0 +1,219 @@
+<template>
+  <div id="user-merge">
+    <div id="generalInfo">
+      <h3>Semi-automatic Merging</h3>
+      <h4>Still under development</h4>
+      <p>
+        You are in the semi-automatic merging mode. This means you can
+        cherry-pick the changes you want to apply. We provide different views on
+        the changes:
+      </p>
+    </div>
+
+    <template v-for="(reaction, index) in reactionsArr" :key="index">
+      <div class="card text-center" v-if="currentSlide == index">
+        <div class="card-header">Featured</div>
+        <div class="card-body row g-0 p-0">
+          <div class="col-8 bivesGraph" :id="`bivesGraph-${index}`"></div>
+          <div class="col-4 changeList" :id="`changeList-${index}`">
+            List changes here instead of popup?
+          </div>
+        </div>
+        <div class="card-footer text-muted">Show math here?</div>
+      </div>
+    </template>
+
+    <!-- Slide buttons -->
+    <div class="btn btn-primary" @click="this.currentSlide++">Up</div>
+    <div class="btn btn-primary" @click="this.currentSlide--">Down</div>
+
+    <!--     <Carousel
+      :decArr="decisionArr"
+      @arrChanged="decisionArr = $event"
+      @slideChange="currentSlide = $ent"
+      @gotOldDoc="oldDocument = $event"
+      @gotNewDoc="newDocument = $event"
+    /> -->
+    <Selection
+      v-if="!hide"
+      :decArr="decisionArr"
+      :slideChng="currentSlide"
+      :oldDoc="oldDocument"
+      :newDoc="newDocument"
+    />
+  </div>
+  <div id="devOutput" v-if="dev">
+    <h3>Dev mode is active!</h3>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { callDiVil } from "../../DiVil/javascriptAndCss/init";
+/* import Carousel from "./Carousel.vue"; */
+import Selection from "./Selection.vue";
+
+//import dev data
+/* import devDataJson from "/dev/navicenta/sbgnJson.json";
+import devDataXmlDiff from "raw-loader!/dev/navicenta/xmlDiff.xml";
+import version1 from "raw-loader!/dev/navicenta/version1.xml";
+import version2 from "raw-loader!/dev/navicenta/version2.xml"; */
+import devDataJson from "/dev/dupreez_6-7/sbgnJson.json";
+import devDataXmlDiff from "raw-loader!/dev/dupreez_6-7/xmlDiff.xml";
+import version1 from "raw-loader!/dev/dupreez_6-7/dupreez6.xml";
+import version2 from "raw-loader!/dev/dupreez_6-7/dupreez7.xml";
+
+export default {
+  name: "user-merge",
+  components: {
+    /*     Carousel,*/
+    Selection,
+  },
+  data() {
+    return {
+      /* START - dev vars */
+      json: devDataJson,
+      xmlDiff: devDataXmlDiff,
+      updates: 0,
+      inserts: 0,
+      deletes: 0,
+      moves: 0,
+      v1: version1,
+      v2: version2,
+      hide: true,
+      /* END */
+      decisionArr: [],
+      reactionsArr: [],
+      currentSlide: 0,
+      oldDocument: null,
+      newDocument: null,
+      dev: true, //flag for development, use sample data
+    };
+  },
+  watch: {
+    currentSlide: {
+      handler: function () {
+        if (this.currentSlide == -1)
+          this.currentSlide = this.reactionsArr.length - 1;
+        if (this.currentSlide >= this.reactionsArr.length)
+          this.currentSlide = 0;
+      },
+    },
+    decisionArr: {
+      handler: function () {},
+    },
+    newDocument: {
+      handler: function () {},
+    },
+  },
+  mounted() {
+    //callDivil();
+    /*  this.$root.$on("arrChanged", (data) => {
+      this.decisionArr = data;
+      console.log(data);
+    });
+    this.$root.$on("slideChange", (data) => {
+      this.currentSlide = data;
+      console.log(data);
+    });
+    this.$root.$on("gotOldDoc", (data) => {
+      this.oldDocument = data;
+      console.log(data);
+    });
+    this.$root.$on("gotNewDoc", (data) => {
+      this.newDocument = data;
+      console.log(data);
+    }); */
+
+    //check for dev mode
+    if (this.dev) {
+      console.log("Dev Mode is active!");
+      console.log(this.json.links, this.json.nodes, this.xmlDiff);
+
+      //compute change stats
+      console.log(this.xmlDiff, typeof this.xmlDiff);
+      //let xmlLines: string[] ;
+
+      //console.log(JSON.parse(this.json));
+
+      //compute reaction view
+      /*
+       * To go through each single change might be cumbersum.
+       * With this view we split the network into each reaction.
+       * Reactions with changes can than be viewed.
+       */
+      this.json.nodes.forEach((node) => {
+        if (node.id.startsWith("r") && node.bivesChange != "nothing") {
+          //every reaction ID starts with r
+          let reactionNodes = [node];
+          let reactionLinks = [];
+          //add reaction to Array, include all link and participants
+          this.json.links.forEach((link) => {
+            if (link.target == node.id || link.source == node.id) {
+              reactionLinks.push(link);
+
+              //add other participant of link
+              let addNode, addNodeId;
+              if (link.target == node.id) {
+                addNodeId = link.source;
+              } else addNodeId = link.target;
+
+              addNode = this.json.nodes.find((n) => n.id == addNodeId);
+              console.log(addNodeId, addNode);
+              reactionNodes.push(addNode);
+            }
+          });
+
+          this.reactionsArr.push({
+            bivesChange: node.bivesChange,
+            nodes: reactionNodes,
+            links: reactionLinks,
+          });
+        }
+      });
+
+      console.log("reactionsArr: ", this.reactionsArr);
+    }
+    /* 
+    function getNode(nodeID) {
+      console.log(this.json);
+      let node = this.json.nodes.find((node) => node.id == nodeID);
+      console.log(node);
+      return node;
+    } */
+  },
+  updated() {
+    callDiVil(
+      this.reactionsArr[this.currentSlide],
+      this.xmlDiff,
+      this.v1,
+      this.v2,
+      "bivesGraph-" + this.currentSlide
+    );
+  },
+};
+</script>
+
+<style>
+.bivesGraph {
+  min-height: 300px;
+  background-color: lightgray;
+}
+
+/*bives-colors*/
+.delete-color {
+  color: #d66a56;
+}
+
+.insert-color {
+  color: #76d6af;
+}
+
+.move-color {
+  color: #8e67d6;
+}
+
+.update-color {
+  color: #d6d287;
+}
+</style>
