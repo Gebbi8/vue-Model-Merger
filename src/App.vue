46f4734c5e13cf0fd6d12d30e08d9b04c3d4caaa
tgeb <tom.gebhardt@uni-rostock.de>
2022-04-14 10:54:06 +0200

    still initializing

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,151 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="topLayer" class="d-flex flex-column h-100">
+    <div class="custom-color-1" id="head">
+      <div class="container">
+        <div class="row">
+          <h1 class="col-8">Model Merger</h1>
+          <div class="col-4 d-flex align-items-center justify-content-end">
+            <button
+              type="button"
+              class="btn btn-light btn-sm"
+              v-on:click="btnSwitchView"
+              v-if="true"
+            >
+              Switch Mode
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="custom-color-2" id="intro">
+      <div class="container">
+        <div class="row justify-content-center">
+          <p class="col-8">
+            Model development is often a non-linear and decentralised process.
+            It requries time consuming effort to gather the necessary
+            information from literature, experimental data and databases. The
+            information are either collected in tables or directly put into a
+            model using common tools (e. g. CellDesigner). Subseqeuntly, the
+            collaborative nature of modelling requires one partner to collect
+            and combine the gathered information. Our merging tool aims to ease
+            this process by enabling the combination of two model versions.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="custom-color-2" id="center">
+      <div class="container">
+        <user-merge v-if="!simple" />
+        <simple-merge v-else />
+      </div>
+    </div>
+    <div class="footer mt-auto custom-color-3" id="foot">
+      <div class="container">
+        <!--Image-->
+        <div class="row">
+          <div class="col-2 d-flex align-items-center">
+            <a
+              href="https://integrative-pathway-models.de/"
+              title="INCOME-Website"
+            >
+              <img src="./assets/csm_logo_INCOME11.png" class="img-fluid" alt />
+            </a>
+          </div>
+          <div class="col-10 d-flex align-items-end flex-column" id="contact">
+            <h5>Contact</h5>
+            <p>Tom Gebhardt</p>
+            <p>tom.gebhardt@uni-rostock.de</p>
+            <p>https://www.sbi.uni-rostock.de/</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 d-flex justify-content-start">
+            The project was funded by the BMBF (FKZ: 01ZX1705C)
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+//import Selection from './components/Selection.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import SimpleMerge from "./components/SimpleMerge.vue";
+import UserMerge from "./components/UserMerge.vue";
+//import { useRoute } from "vue-router";
+
+export default {
+  name: "app",
+  components: {
+    SimpleMerge,
+    UserMerge,
+  },
+  data() {
+    return {
+      simple: true,
+      job: "fakeJob",
+      //job: this.$route.query.jobID,
+    };
+  },
+  methods: {
+    btnSwitchView: function () {
+      this.simple = !this.simple;
+    },
+  },
+};
+
+// export default {
+//   name: 'App',
+//   components: {
+//     //HelloWorld,
+//     SimpleMerge,
+//     //Selection,
+//     UserMerge
+//   }
+// }
+</script>
+
 <style>
-@import "@/assets/base.css";
+.row > * {
+}
+</style>
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+<style scoped>
+.custom-color-1 {
+  background-color: #365b9e;
+  color: #feffff;
 }
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.custom-color-2 {
+  background-color: #e5e5e5;
+  color: #17252a;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.custom-color-3 {
+  background-color: white;
+  color: #17252a;
 }
 
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
+#intro {
+  padding-top: 1rem;
 }
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
+#center {
+  padding-bottom: 1em;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+#head {
+  padding-top: 1em;
+  padding-bottom: 1rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#foot {
+  padding-top: 1em;
+  padding-bottom: 1em;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#contact > p {
+  margin-bottom: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-import router from "./router";
+
+//import Page1 from "./views/Page1";
+//import Page2 from "./views/Page2";
+//import Page3 from "./views/Page3";
+
+const routes = [
+    //{ path: "/", component: Page1 },
+    //{ path: "/page2", component: Page2 },
+    //{ path: "/page3", component: Page3 }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
 
 const app = createApp(App);
 
 app.use(router);
-
 app.mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js"
