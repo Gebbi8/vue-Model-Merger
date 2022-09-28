2eeecefbefd0d0627bcea3ca8c5ae48a0a576d94
tgeb <tom.gebhardt@uni-rostock.de>
2022-09-28 21:10:36 +0200

    working on the top level change view

--- src/components/UserMerge.vue
+++ src/components/UserMerge.vue
@@ -11,26 +11,60 @@
     </div>
 
     <div class="row justify-content-evenly">
-      <input type="radio" class="btn-check col-2" name="options" id="option1" autocomplete="off">
+      <input type="radio" class="btn-check col-2" name="options" id="option1" autocomplete="off" checked>
       <label @click="view = 'model'" class="btn btn-outline-primary col-2" for="option1">Model</label>
 
       <input type="radio" class="btn-check col-2" name="options" id="option2" autocomplete="off">
       <label @click="view = 'species';" class="btn btn-outline-primary col-2" for="option2">Species</label>
 
-      <input type="radio" class="btn-check col-2" name="options" id="option3" autocomplete="off"  checked>
+      <input type="radio" class="btn-check col-2" name="options" id="option3" autocomplete="off"  >
       <label @click="view = 'reactions'" class="btn btn-outline-primary col-2" for="option3">Reactions</label>
 
-      <input type="radio" class="btn-check col-2" name="options" id="option4" autocomplete="off">
+      <input type="radio" class="btn-check col-2" name="options" id="option4" autocomplete="off" disabled>
       <label @click="view = 'parameters'" class="btn btn-outline-primary col-2" for="option4">Parameters</label>
     </div>
+
+    <div  v-if="view == 'model'">
+      <p>Looking at the changes in model describtion, name, ...</p>
+        <ul class="list-group">
+          <template v-for="(element, index) in modelArr" :key="index">
+          <li v-if="element.id != 'listOfSpecies' && element.id != 'listOfReactions'" class="list-group-item" :id="`${element.id}`">
+            <h5>{{ element.id }}</h5>
+            <ul class="list-group">
+              <template v-for="(el, key) in element.attr" :key="key">
+                <li class="list-group-item" ><b>{{ key }}</b>: {{ el }}</li>
+              </template>
+            </ul>
+          </li>
+        </template>
+      </ul>
+    </div>
+
+
+    <div v-if="view == 'species'">
+      <template v-for="(species, index) in speciesArr" :key="index">
+        <div class="card" v-if="currentSlide == index">
+          <div class="card-header text-center">Featured</div>
+          <div class="card-body row g-0 p-0">
+            <div class="col-8 bivesGraph" :id="`bivesGraphSpecies-${index}`"></div>
+            <div class="col-4 changeList" :id="`changeListSpecies-${index}`">
+              Species focus view, just like reaction view, but the subgraph is computed for d=2
+              {{ this.currentSlide }}
+            </div>
+          </div>
+          <div class="card-footer text-muted">Show math here?</div>
+        </div>
+      </template>
+    </div>
+
 
     <div v-if="view == 'reactions'">
       <template v-for="(reaction, index) in reactionsArr" :key="index">
         <div class="card" v-if="currentSlide == index">
           <div class="card-header text-center">Featured</div>
           <div class="card-body row g-0 p-0">
-            <div class="col-8 bivesGraph" :id="`bivesGraph-${index}`"></div>
-            <div class="col-4 changeList" :id="`changeList-${index}`">
+            <div class="col-8 bivesGraph" :id="`bivesGraphReaction-${index}`"></div>
+            <div class="col-4 changeList" :id="`changeListReaction-${index}`">
               List changes here instead of popup?
             </div>
           </div>
@@ -61,6 +95,8 @@ import * as divilApi from "../../DiVil/javascriptAndCss/init";
 
 import devDataJson from "/dev/dupreez_6-7/sbgnJson.json";
 import testArr from "/dev/dupreez_6-7/decisionArray.js";
+import modelTestArr from "/dev/dupreez_6-7/decisionArray.js";
+import { list } from "postcss";
 
 
 export default {
@@ -72,16 +108,18 @@ export default {
   data() {
     return {
       json: devDataJson,
-      view: "reactions",
+      view: "model",
       v1: null,
       v2: null,
       hide: true,
+      modelArr: [],
       decisionArr: testArr, // null,
       reactionsArr: [],
+      speciesArr: [],
       structuredData: null,
       currentSlide: 0,
       oldDocument: null,
-      newDocument: null,
+      newDocument: null, //should also be given to subcomponent to avoid parsing it twice!
       dev: true, //flag for development, use sample data
     };
   },
@@ -89,19 +127,140 @@ export default {
     getId: function (line){
           var regex = new RegExp('id="(.*?)"', 'g');
           return regex.exec(line)[1];
+    },
+    getModelChanges: function () {
+      let modelChanges = [];
+      let xmlLines = this.xmlDiff.split(/\r?\n/);
+      let type;
+      xmlLines.forEach(line => {
+        if(line.includes("triggeredBy="));
+        else if(line.includes("<move>")) type = null;
+        else if(line.includes("<insert>")){
+              type = 'i';
+            }
+        else if(line.includes("<delete>")){
+              type = 'd';
+            }
+        else if(line.includes("<update>")){
+              type = 'u';
+            }
+        else if(line.includes('/sbml[1]/model[1]\"') && type != null){
+          let parser = new DOMParser();
+          line = parser.parseFromString(line, "application/xml");
+          let p = {};
+          p["type"] = type;
+          p["target"] = line.firstChild.localName;
+          
+          //console.debug(line);
+          //alert();
+
+          let attr = line.firstChild.attributes;
+
+          for(let i = 0; i < attr.length; i++){
+            p[attr[i].localName] = attr[i].value;
+          }
+
+          modelChanges.push(p);
+        }
+      });
+      
+      return modelChanges;
+    },
+
+    getModelAttr: function (){
+
+      // gets Attributes on SBML tag
+      let attr = [];
+      let modelAttr = [];
+      attr = this.getAllNodeAttr(this.newDocument.firstChild);
+      modelAttr.push({'id': 'sbmlAttr', 'attr': attr});
+
+      //get al Attributes of model
+      attr = this.getAllNodeAttr(this.newDocument.firstChild.children.item('model'));
+      modelAttr.push({'id': 'modelAttr', 'attr': attr});
+
+      //get all childrenTags of <model>
+      let lists = this.newDocument.firstChild.children.item(0).children;
+      console.debug("...", lists);
+      for(let i = 0; i < lists.length; i++){
+        let p = {};
+        p["class"] = "list";
+        p["id"] = lists[i].localName;
+        
+        modelAttr.push(p);
+      }
+
+      return modelAttr;
+    },
+
+    combineModelAttrWithChanges: function(){
+      let changes = this.getModelChanges();
+      let attributes = this.getModelAttr();
+
+
+      
+      changes.forEach((c) => {
+        if(c.target == "attribute"){
+          if(c.type == "u"){
+            let el;
+            if(c.newPath == "/sbml[1]/model[1]"){
+              console.debug(attributes);
+              let target = attributes.find(a => a.id === "modelAttr");
+              let el = {};
+              el["changeID"] = c.id;
+              el["changeType"] = "u";
+              el["oldValue"] = c.oldValue;
+              el["newValue"] = c.newValue;
+              
+              target.attr[c.name] = el;
+
+              console.debug(attributes);
+            } else if(c.newPath == "/sbml[1]"){
+
+            }
+            
+          } else if(c.type == "i"){
+
+          } else if(c.type == "d"){
+
+          }
+        }
+        else if(c.target == "node"){
+
+        }
+        else alert("can't handle " + c.target + " change on model level");
+      })
+
+      this.modelArr = attributes;
+      
+    },
+
+    getAllNodeAttr: function(node) {
+      let attrMap = node.attributes;
+      let p = {};
+      for(let i = 0; i < attrMap.length; i++){
+       //let p;
+       p[attrMap[i].localName] = attrMap[i].value;
+       //attr.push(p);
+      }
+
+      return p;
     }
   },
   watch: {
     view:{
       handler: function(){
-        divilApi.removeDivilDiv("bivesGraph-" + this.currentSlide);
+        divilApi.stopD3ForceOfDivil("bivesGraph-" + this.currentSlide);
       }
     },  
     currentSlide: {
       handler: function () {
+        let length;
+        if(this.view == 'species') length = this.speciesArr.length;
+        else length = this.reactionsArr.length;
         if (this.currentSlide == -1)
-          this.currentSlide = this.reactionsArr.length - 1;
-        if (this.currentSlide >= this.reactionsArr.length)
+          this.currentSlide = length - 1;
+        if (this.currentSlide >= length)
           this.currentSlide = 0;
       },
     },
@@ -144,6 +303,9 @@ export default {
           this.v2 = responses[1].value.data;
           this.xmlDiff = responses[2].value.data;
 
+          let parser = new DOMParser();
+          this.newDocument = parser.parseFromString(this.v2, "application/xml");
+          
           //compute reaction view
           /*
            * To go through each single change might be cumbersum.
@@ -164,9 +326,14 @@ export default {
                   if (link.target == node.id) {
                     addNodeId = link.source;
                   } else addNodeId = link.target;
-                  addNode = this.json.nodes.find((n) => n.id == addNodeId);
+                  
                   //console.log(addNodeId, addNode);
-                  reactionNodes.push(addNode);
+                  if(!reactionNodes.some(rN => rN.id === addNodeId)){
+                    console.info(reactionNodes, addNodeId);
+                    //alert("check nodes");
+                    addNode = this.json.nodes.find((n) => n.id == addNodeId);
+                    reactionNodes.push(addNode);
+                  }
                 }
               });
               this.reactionsArr.push({
@@ -175,6 +342,61 @@ export default {
                 nodes: reactionNodes,
                 links: reactionLinks,
               });
+            } else if (node.id.startsWith("s") && node.bivesChange != "nothing"){ //want the subnetwor for the distance 2
+              let speciesNodes = [node];
+              let speciesLinks = [];
+              //add reaction to Array, include all link and participants
+              this.json.links.forEach((link) => {
+                if (link.target == node.id || link.source == node.id) {
+                  speciesLinks.push(link);
+                  //add other participant of link
+                  let addNode, addNodeId;
+                  if (link.target == node.id) {
+                    addNodeId = link.source;
+                  } else addNodeId = link.target;
+                  
+                  if(!speciesNodes.some(sN => sN.id === addNodeId)){
+                    addNode = this.json.nodes.find((n) => n.id == addNodeId);
+                    speciesNodes.push(addNode);
+                  }
+                   this.json.links.forEach((link2) => {
+
+                    if(link2.target == addNodeId || link2.source == addNodeId){
+
+                    
+                    
+                    
+
+                    //alert("before before");
+
+                    if(!speciesLinks.some(sL => sL.path === link2.path))
+                    {
+                      speciesLinks.push(link2);
+
+                      let addNode2, addNodeId2;
+                      if(link2.target == addNodeId) addNodeId2 = link2.source;
+                      else addNodeId2 = link2.target;
+
+                      if(!speciesNodes.some(sN => sN.id === addNodeId2))
+                      {
+                        //alert("check nodes"); 
+                        addNode2 = this.json.nodes.find((n) => n.id === addNodeId2);
+                        speciesNodes.push(addNode2);
+                       }
+                      //addNode2 = this.json.nodes.find((n) => n.id == addNodeId2);
+                      //speciesNodes.push(addNode2);*/
+                    }
+                  }
+                  }) 
+                }
+              });
+       
+              this.speciesArr.push({
+                centralNode: node.path,
+                bivesChange: node.bivesChange,
+                nodes: speciesNodes,
+                links: speciesLinks,
+              });
             }
           });
 
@@ -182,7 +404,9 @@ export default {
           //init computed divil data
           this.structuredData = divilApi.initDivil(this.xmlDiff, this.v1, this.v2);
 
-          console.info(this.decisionArr);
+          //console.info(this.decisionArr);
+          console.info(this.speciesArr);
+          console.info(this.reactionsArr);
 
           /**********  Creation of Decision Array, works fine currently not used for development   
           //create decision array
@@ -214,34 +438,44 @@ export default {
           if(this.decisionArr[0][0] == "bivesPatch") this.decisionArr.shift();
           console.info(this.decisionArr);
           *********************************/
+          this.combineModelAttrWithChanges();
+
         })
         .catch(error => {
-          console.error(error.message)
+          console.error(error.message);
         });
-
 
 
       console.log("Dev Mode is active!");
       console.log("reactionsArr: ", this.reactionsArr);
     }
-    /* 
-    function getNode(nodeID) {
-      console.log(this.json);
-      let node = this.json.nodes.find((node) => node.id == nodeID);
-      console.log(node);
-      return node;
-    } */
   },
   updated() {
-    if(this.view == 'reactions'){
-      console.log(this.v1, this.v2);
+    if(this.view === "reactions" || this.view === "species"){
+      let currentSlide;
+      let container;
+      let changeList;
+      if(this.view == 'reactions'){
+        currentSlide = this.reactionsArr[this.currentSlide];
+        container = "bivesGraphReaction-" + this.currentSlide;
+        changeList = "changeListReaction-" + this.currentSlide;
+      } else if(this.view == 'species'){
+        currentSlide = this.speciesArr[this.currentSlide];  //OBACHT: this.currentSlide is atm the some for both views
+        container = "bivesGraphSpecies-" + this.currentSlide;
+        changeList = "changeListSpecies-" + this.currentSlide;
+      }
+    
+
+
+      console.info(currentSlide);
+
       divilApi.callDiVil(
-        this.reactionsArr[this.currentSlide],
+        currentSlide,
         this.xmlDiff,
         this.v1,
         this.v2,
-        "bivesGraph-" + this.currentSlide,
-        "changeList-" + this.currentSlide,
+        container,
+        changeList,
         this.structuredData
       );
       MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
