9de4dabe01ba2e9107b00930e5af4eaf03852760
tgeb <tom.gebhardt@uni-rostock.de>
2023-01-27 23:30:15 +0100

    dded missing file

---
+++ src/components/ListDecision.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="btn-group col-1" role="group" :id="btngrp" aria-label="Toggle group">
+    <input type="radio" class="btn-check" :name="btnradio" :id="bV1" autocomplete="off" @click="this.emitDecision(0)" :checked="this.decision == 0">
+    <label class="btn btn-outline-primary" :for="`${changeID}-bV1`">V1</label>
+
+    <!--     <input type="radio" class="btn-check" :name="`${changeID}-btnradio`" :id="`${changeID}-bNew`" autocomplete="off" @click="this.emitDecision(changeID, 2)" disabled :checked="d == 2">
+        <label class="btn btn-outline-primary" :for="`${changeID}-bNew`">New</label> -->
+
+    <input type="radio" class="btn-check" :name="btnradio" :id="bV2" autocomplete="off" @click="this.emitDecision(1)" :checked="this.decision == 1">
+    <label class="btn btn-outline-primary" :for="`bV2`">V2</label>
+</div>
+</template>
+
+    
+<script>
+export default {
+    emits: ['decision'],
+    props: {
+        decisionArr: Object,
+        list: Array,
+    },
+    data() {
+        return{
+            decision: -1
+        }
+    },
+    mounted() {
+        console.debug(this.list);
+        alert("asd");
+        let d = this.list[0];
+        for(let n = 0; n < list.length; n++){
+            console.debug(list[n]);
+            alert();
+        }
+    },
+    methods: {
+        emitDecision: function (dec) {
+
+            for (let n = 0; n < this.list.length; n++) {
+                this.$emit('decision', list[n][0], dec);
+            }
+        }
+    }
+
+}
+</script>
---
+++ src/components/LocalFiles.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div id="fileUpload" class="row custom-color-3">
+      <div id="uploadFirst" class="col-sm-6 custom-color-1">
+        <div class="input-group mb-3">
+          <input type="file" class="form-control" id="inputGroupFile02" ref="file1" v-on:change="handleFileUpload(0)" />
+        </div>
+      </div>
+      <div id="uploadSecond" class="col-sm-6 custom-color-2">
+        <div class="input-group mb-3">
+          <input type="file" class="form-control" id="inputGroupFile02" ref="file2" v-on:change="handleFileUpload(1)" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["file1", "file2"],
+  props: {
+  },
+  data() {
+    return {
+      file1: null,
+      file2: null
+    }
+  },
+  methods: {
+    handleFileUpload(v) {
+      if (v == 0) {
+        this.file1 = this.$refs.file1.files[0];
+        console.log(this.$refs.file1.files[0]);
+        //emit
+        this.$emit('file1', this.file1);
+      } else if (v == 1) {
+        this.file2 = this.$refs.file2.files[0];
+        console.log(this.$refs.file2.files[0]);
+        this.$emit('file2', this.file2);
+      } else console.error(v + "  is not a valid version number");
+    },
+  }
+}
+</script>
