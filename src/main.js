c0c1243610c70b91056e82597aa5f97cb8be6e30
tgeb <tom.gebhardt@uni-rostock.de>
2023-01-30 16:23:10 +0100

    working on deploy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import { createApp } from "vue";
+//import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+const app = Vue.createApp(App);
 
 app.use(router);
 
