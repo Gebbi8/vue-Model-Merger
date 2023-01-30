8291a0b16da6b5dcdc39fe43f1aee86f91d45262
tgeb <tom.gebhardt@uni-rostock.de>
2023-01-30 14:57:20 +0100

    setting base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      base: '/vue3-merger/',
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
