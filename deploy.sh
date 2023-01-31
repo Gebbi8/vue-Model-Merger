d7329ece0a431c9e27c3a838abf7d4a9be22b2a0
tgeb <tom.gebhardt@uni-rostock.de>
2023-01-31 20:57:11 +0100

    working on vite deploy

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Gebbi8/vue3-merger.git main:gh-pages
+git push -f git@github.com:Gebbi8/vue-Model-Merger.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const path = require('path');
+//const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      base: '/vue3-merger/',
-      root: path.resolve(__dirname, 'src'),
-      //"@": fileURLToPath(new URL("./src", import.meta.url)),
-      vue: path.resolve('./node_modules/vue'),
-      //'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-
-    },
+  build: {
+    base: "https://gebbi8.github.io/vue-Model-Merger/",
+    //outDir: "./sbml-merger/",
+    //sourcemap: true,
+    emptyOutDir: true,
+    /*     rollupOptions: {
+          output: {
+            entryFileNames: "[name].js",
+            chunkFileNames: "[name].js",
+            assetFileNames: "[name].txt"
+          }
+        } */
   },
+  /*   resolve: {
+      alias: {
+        //base: '/vue3-merger/',
+        //root: path.resolve(__dirname, 'src'),
+        //"@": fileURLToPath(new URL("./src", import.meta.url)),
+        //vue: path.resolve('./node_modules/vue'),
+        //'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+  
+      },
+    }, */
 });
