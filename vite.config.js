e7af3414ada89288ebe582348299b635a768de27
tgeb <tom.gebhardt@uni-rostock.de>
2023-01-31 13:37:43 +0100

    working on vite deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const path = require('path')
+const path = require('path');
+
+console.debug(path);
 
 // https://vitejs.dev/config/
 export default defineConfig({
