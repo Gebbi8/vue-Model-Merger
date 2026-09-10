3f879fc397bbdb012901d7de26b52780588d8057
Tom Gebhardt <tom.gebhardt@uni-greifswald.de>
2026-09-10 11:52:43 +0200

    toolchain update

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,21 @@
-//import { fileURLToPath, URL } from "url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-//const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
-    base: '/',
-    //outDir: "./sbml-merger/",
-    //sourcemap: true,
     emptyOutDir: true,
-    /*     rollupOptions: {
-          output: {
-            entryFileNames: "[name].js",
-            chunkFileNames: "[name].js",
-            assetFileNames: "[name].txt"
-          }
-        } */
   },
-  /*   resolve: {
-      alias: {
-        //base: '/vue3-merger/',
-        //root: path.resolve(__dirname, 'src'),
-        //"@": fileURLToPath(new URL("./src", import.meta.url)),
-        //vue: path.resolve('./node_modules/vue'),
-        //'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-  
+  server: {
+    proxy: {
+      // BiVeS merge backend (PHP). In production the app is deployed
+      // alongside these scripts on merge-proto; in dev we forward to it.
+      "/bives": {
+        target: "https://merge-proto.bio.informatik.uni-rostock.de",
+        changeOrigin: true,
+        secure: true,
       },
-    }, */
+    },
+  },
 });
